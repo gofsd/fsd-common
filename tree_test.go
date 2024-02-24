acb681c58152d7c98e7dbc9c9a8e173f423ecf47
gofsd <madi.nickname@gmail.com>
2024-02-25 01:56:27 +0200

    upd types

--- tree_test.go
+++ tree_test.go
@@ -72,6 +72,7 @@ func TestClient_TreeCmd(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := c.TreeCmd(tt.args.command)
+
 			if (err != nil) != tt.wantErr {
 
 				t.Errorf("Client.TreeCmd() error = %v, wantErr %v", err, tt.wantErr)
