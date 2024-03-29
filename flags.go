1f88f055fc3c5b5f2c9a9736b5a093cae05549c6
gofsd <madi.nickname@gmail.com>
2024-03-29 15:19:21 +0200

    upd

--- flags.go
+++ flags.go
@@ -1,19 +1,6 @@
 package types
 
 type RootFlags struct {
-	Action, Command, Output uint8
-	UserID, EntityID        uint32
-	Flag                    []string
-}
-
-func (rf *RootFlags) ForEach(callback func(key, value string)) (e error) {
-	kv := KV{}
-	for _, v := range rf.Flag {
-		e = FromJson(&v, &kv)
-		if e != nil {
-			return e
-		}
-		callback(kv.Key, kv.Value)
-	}
-	return e
+	Action, Output   uint8
+	UserID, EntityID uint32
 }
