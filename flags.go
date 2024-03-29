f95bb336606f064b17ed0aed2cb6496f27918e5b
gofsd <madi.nickname@gmail.com>
2024-03-30 00:41:08 +0200

    upd

--- flags.go
+++ flags.go
@@ -1,8 +1,8 @@
 package types
 
 type RootFlags struct {
-	Action   uint8  `json:"action"`
-	Output   uint8  `json:"output"`
-	UserID   uint32 `json:"user"`
-	EntityID uint32 `json:"entity"`
+	Action   uint8  `json:"action" mapstructure:"action"`
+	Output   uint8  `json:"output" mapstructure:"output"`
+	UserID   uint32 `json:"user" mapstructure:"user"`
+	EntityID uint32 `json:"entity" mapstructure:"entity"`
 }
